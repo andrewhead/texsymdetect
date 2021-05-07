150df457ac6d8aa832f335157547e9adbc5c09e3
Andrew Head <head.andrewm@gmail.com>
2021-05-07 06:49:19 -0700

    Add setup.py to support installation from GitHub repo

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="texsymdetect",
+    version="0.0.1",
+    packages=[
+      "texsymdetect.client",
+    ],
+    license="Apache License 2.0",
+    long_description=open("README.md").read(),
+    url="https://github.com/andrewhead/texsymdetect",
+    install_requires=[
+        "requests>=2.0.0,<3.0.0"
+    ],
+)
